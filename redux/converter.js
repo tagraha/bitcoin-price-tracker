b12f20bcfd0a26483b4957d89a19f28d5d773755
Tirta Nugraha <dev.nugrata@gmail.com>
2018-03-14 22:41:31 +0700

    other(action): adding converter action

--- redux/converter.js
+++ redux/converter.js
@@ -1,11 +1,15 @@
 import "isomorphic-fetch";
+import get from 'lodash/get';
 
 // Actions
 const HANDLE_CHANGE = "HANDLE_CHANGE";
 const CONVERT_BTC_USD = "CONVERT_BTC_USD";
 
 const initialObj = {
-  inputText: ''
+  inputText: '',
+  validInput: 0,
+  buyPrice: 0,
+  userMoney: 0
 }
 
 // Reducer
@@ -16,8 +20,14 @@ export default function reducer(state = initialObj, action) {
     }
 
     case CONVERT_BTC_USD: {
+      const inputNumber = parseFloat(get(action, 'payload'));
+      const price = parseFloat(get(action, 'price'));
+      const dollar = parseFloat(inputNumber * price);
       return {
-        ...state
+        ...state,
+        validInput: inputNumber,
+        buyPrice: price,
+        userMoney: precisionRound(dollar, 3)
       }
     }
 
@@ -29,12 +39,20 @@ export default function reducer(state = initialObj, action) {
 
 // Action Creators
 const updateBtc = (text) => ({ type: HANDLE_CHANGE, payload: text });
-const convertBTC = (text) => ({ type: HANDLE_CHANGE, payload: text });
+const convertBTC = (input, price) => ({ type: CONVERT_BTC_USD, payload: input, price });
 
+
+// Exported Action
 export const handleInputChange = (text) => (dispatch, getState) => {
   return dispatch(updateBtc(text));
 };
 
-export const convert = (btc) => (dispatch, getState) {
-  return dispatch(convertBTC(btc));
+export const convertBtc = (btc, buyPrice) => (dispatch, getState) => {
+  return dispatch(convertBTC(btc, buyPrice));
+}
+
+// perks
+function precisionRound(number, precision) {
+  var factor = Math.pow(10, precision);
+  return Math.round(number * factor) / factor;
 }
